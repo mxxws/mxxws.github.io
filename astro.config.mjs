7dfd0c0808508e6632eef0c51fb1bd6773110758
ccafder <ccafder@192.168.5.13>
2026-04-26 10:12:08 +0800

    Initial commit with Astro blog template

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	// 请将此处的 site 替换为你的 GitHub Pages 域名，例如 'https://<用户名>.github.io'
+	site: 'https://example.com',
+	// 如果你的仓库名不是 <用户名>.github.io，请取消注释并将其设置为 '/<仓库名>'
+	// base: '/myblog',
+	integrations: [mdx(), sitemap()],
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: 'Atkinson',
+			cssVariable: '--font-atkinson',
+			fallbacks: ['sans-serif'],
+			options: {
+				variants: [
+					{
+						src: ['./src/assets/fonts/atkinson-regular.woff'],
+						weight: 400,
+						style: 'normal',
+						display: 'swap',
+					},
+					{
+						src: ['./src/assets/fonts/atkinson-bold.woff'],
+						weight: 700,
+						style: 'normal',
+						display: 'swap',
+					},
+				],
+			},
+		},
+	],
+});
